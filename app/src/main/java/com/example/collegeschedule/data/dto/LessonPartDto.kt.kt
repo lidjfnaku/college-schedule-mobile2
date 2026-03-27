6bcfeef318cfb867c412da4c6d49df8a3e4d33c5
lidjfnaku <lidjfnaku@gmail.com>
2026-03-28 01:09:40 +0700

    Add: DTO models LessonDto.kt

---
+++ app/src/main/java/com/example/collegeschedule/data/dto/LessonPartDto.kt.kt
@@ -0,0 +1,9 @@
+package com.example.collegeschedule.data.dto
+
+data class `LessonPartDto.kt`(
+    val subject: String,
+    val teacher: String,
+    val teacherPosition: String,
+    val classroom: String,
+    val building: String,
+    val address: String )
