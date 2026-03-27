26d0335a724dcc9194e8e4ef7362c6f15eec0b3d
lidjfnaku <lidjfnaku@gmail.com>
2026-03-28 02:09:11 +0700

    Add: ScheduleRepository.kt

--- app/src/main/java/com/example/collegeschedule/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/collegeschedule/data/network/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://10.0.2.2:5268/")
+        .baseUrl("http://10.0.2.2:5203/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     val api: ScheduleApi = retrofit.create(ScheduleApi::class.java)
